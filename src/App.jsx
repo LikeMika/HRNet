db6f5e1a566b600183724dd15f93b2fe4d839a4c
Michael Bricout <bricout.michael@gmail.com>
2025-07-24 04:15:56 +0200

    Project initialization + react install + structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import CreateEmployee from './pages/CreateEmployee'
+import EmployeeList from './pages/EmployeeList'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<CreateEmployee />} />
+        <Route path="/employee-list" element={<EmployeeList />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
